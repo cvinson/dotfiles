4678c8e8926b363a9d443275fc838930aa30a3fe
Colin Vinson <cvinson86@gmail.com>
2018-09-07 10:05:38 -0500

    Untested setup.sh

---
+++ setup.sh
@@ -0,0 +1,2 @@
+git config --global alias.lg "log --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+brew cask install google-chrome firefox iterm2 spotify yakyak postman
